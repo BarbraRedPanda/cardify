219b84eaa509d014a36242a864126c81add86871
Parsa Khairollahi <parsak9000@gmail.com>
2023-03-10 22:10:19 -0600

    Added Autofill feature for title and (occasionally) author

---
+++ content.js
@@ -0,0 +1,55 @@
+//tries to automatically get the author's name via metadata
+// if it doesn't work, it just goes back to what the old name was
+
+/*function getAuthor() {
+  const metaAuthor = document.querySelector('meta[name="author"]');
+  if (metaAuthor) {
+    const authorName = metaAuthor.getAttribute('content');
+    if (authorName) {
+      return authorName.trim();
+    }
+  }
+
+  const authorElements = document.querySelectorAll('.author, .byline');
+  if (authorElements.length > 0) {
+    const authorName = authorElements[0].textContent.trim();
+    if (authorName) {
+      return authorName;
+    }
+  }
+
+  return "joe";
+}*/
+
+//sends article title to background script
+browser.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  if (message.action === "getTitle") {
+    const pageTitle = getTitle();
+    console.log(pageTitle);
+    sendResponse({value: pageTitle});
+  } else if (message.action === "getAuthor")  {
+    sendResponse({value: getAuthor()});
+  } else if (message.action === "getDate")  {
+    sendResponse({value: getDate()});
+  }
+});
+
+
+function getTitle()  {
+  const metaElement = window.document.querySelector("meta[property*='title']");
+  const title = metaElement.getAttribute('content');
+  return title;
+}
+
+function getAuthor()  {
+  const author = window.document.querySelector("meta[name*='author']").getAttribute('content');
+  if(author) return author; // tests to see if there is an author
+  // if the first method didn't work, looks through a differnt way of storing author (ie for NYT)
+  author = window.document.querySelector("meta[name*='byl']").getAttribute('content');
+  return author;
+}
+
+function getDate()  {
+  const date = window.document.querySelector("meta[property*='time']").getAttribute('content');
+  return date;
+}
--- popup/popup.js
+++ popup/popup.js
@@ -3,18 +3,67 @@
 const cutButton = document.getElementById("cutButton");
 const output = document.getElementById("para");
 
+//creates constant variables that hold the html elements for the input boxes
+const authorInput = document.getElementById("author");
+const dateInput = document.getElementById("date");
+const titleInput = document.getElementById("title");
+const tagInput = document.getElementById("tag");
+
 //instantiates and stores the url of the tab which the popup is running on
 let currentUrl;
+let author = "";
+let date = "";
+var title = ""; 
+let tag = "";
 browser.tabs.query({currentWindow: true, active: true}).then((tabs) => {
-  currentUrl = tabs[0].url;
+    var currentTab = tabs[0];
+    currentUrl = tabs[0].url;
 });
 
 
+// sends a message to content script whenever the popup is opened
+browser.tabs.query({active: true, currentWindow: true}).then(function(tabs) {
+  // Send a message to the content script to retrieve the page title
+  browser.tabs.sendMessage(tabs[0].id, {action: "getTitle"}).then(function(response) {
+    // Update the title input in the popup with the retrieved page title
+    title = response.value;
+
+    titleInput.value = title;
+    console.log(title);
+  });
+  browser.tabs.sendMessage(tabs[0].id, {action: "getAuthor"}).then(function(response) {
+    // Update the author input 
+    author = response.value;
+    authorInput.value = author;
+    console.log(author);
+  });
+  browser.tabs.sendMessage(tabs[0].id, {action: "getDate"}).then(function(response) {
+    // Update the date input 
+    date = response.value;
+    dateInput.value = date;
+    console.log(date);
+  });
+});
+
+//fills in any of the input slots with any information found 
+authorInput.value = author;
+dateInput.value = date;
+titleInput.value = title;
+
 //whenever the cutButton button is clicked, it runs copyCard function
 cutButton.addEventListener("click", (event) =>  {
+    console.log("clicked");
     createCard();
-})
+});
 
+
+//stops scrolling from closing the window
+document.addEventListener('scroll', function(event) {
+    event.preventDefault();
+});
+
+
+//fortnite 
 //returns string of whatever's highlighted
 function getSelectedText() {
     //gets the active tab and returns the currentlyl highlighted text
@@ -27,14 +76,13 @@ function getSelectedText() {
 
 //generates then copies the card to the clipboard
 function createCard() {
-    //creates a blank string that will hold the cites portion of the card
+    
     let cites = '';
     //gets the values of the html elements and stores them
-    let author = document.getElementById("author").value;
-    let title = document.getElementById("title").value;
-    let tag = document.getElementById("tag").value;
-    let date = document.getElementById("date").value;
-
+    author = authorInput.value
+    date = dateInput.value;
+    title = titleInput.value;
+    tag = tagInput.value;
     //creates a date and formats it
     var currentDate = new Date();
     var formattedDate = formatDate(currentDate);
@@ -43,7 +91,7 @@ function createCard() {
     //concatenates information into the cite in the following format:
     // Name (Date) "Title", url. Accessed on Date2. Tag
     cites += author + " (" + date + ") \"" + title + "\", " + currentUrl + ". Accessed on " + formattedDate + 
-            ". " + tag;
+            ". //" + tag;
     
     //consolidates the cites and the evidence under a card
     let card = ""; 
@@ -62,12 +110,8 @@ function copyCard(card) {
     //copies card to clipboard; temporarily changes button's text if it works/fails
     navigator.clipboard.writeText(card).then(function() {
         cutButton.textContent = "success";
-        wait(5000);
-        cutButton.textContent = "Cut";
     }, function(err) {
         cutButton.textContent = "fail";
-        wait(1000);
-        cutButton.textContent = "Cut";
     });
 
 }
@@ -78,6 +122,52 @@ function formatDate(date)  {
     var formattedDate = date.toLocaleDateString('en-US', options);
     return formattedDate;
 }
+
+
+
+/*
+//tries to automatically get the author's name via metadata
+// if it doesn't work, it just goes back to what the old name was
+function getAuthor(currentTab)    {
+
+    if (!currentTab) {
+        console.error("currentTab is not defined");
+        return Promise.reject(new Error("currentTab is not defined"));
+    }
+
+    const defaultAuthor = "Balls";
+
+
+    // Try to get the author from a meta tag
+    const metaCode = 'document.querySelector("meta[name=\'author\']").getAttribute("content")';
+    const metaPromise = browser.tabs.executeScript(currentTab.id, { code: metaCode })
+        .then(result => result[0])
+        .catch(error => console.error(error));
+
+    // Try to get the author from the Open Graph protocol
+    const ogCode = 'document.querySelector("meta[property=\'og:article:author\']").getAttribute("content")';
+    const ogPromise = browser.tabs.executeScript(currentTab.id, { code: ogCode })
+        .then(result => result[0])
+        .catch(error => console.error(error));
+
+    // Try to get the author from the article element
+    const articleCode = 'document.querySelector("article [itemprop=\'author\']")';
+    const articlePromise = browser.tabs.executeScript(currentTab.id, { code: articleCode })
+        .then(result => result[0].textContent.trim())
+        .catch(error => console.error(error));
+
+    // Wait for all promises to resolve and get the first successful result
+    Promise.all([metaPromise, ogPromise, articlePromise])
+        .then(results => {
+        const foundAuthor = results.find(result => result != null && result != "");
+        return foundAuthor || defaultAuthor; // or do something else with the author
+        })
+        .catch(error => {
+            console.error(error);
+            return defaultAuthor;
+        });
+}*/
+
 
 
 
