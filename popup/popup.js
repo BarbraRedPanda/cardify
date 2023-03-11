402ad9854648e5f2e3cbb37769a9a416f4da1de5
Parsa Khairollahi <parsak9000@gmail.com>
2023-03-11 13:17:53 -0600

    Cleaned up code and and comments

--- popup/popup.js
+++ popup/popup.js
@@ -10,40 +10,53 @@ const titleInput = document.getElementById("title");
 const urlInput = document.getElementById("url");
 const tagInput = document.getElementById("tag");
 
-//instantiates and stores the url of the tab which the popup is running on
-let currentUrl;
-let author = "";
-let date = "";
-var title = ""; 
-let tag = "";
-//querys for the active tab 
+/*
+Note: 
+The next few asynchronus calls will autofill the inputs.
+
+This is easier than storing them in global variables since 
+a) the functions are asyncronus
+b) there's no point in global variables if they are going to be varied by the user (if that makes sense)
+*/
+
+
+// Get the tag value from storage and autofills it into the popup
+browser.storage.local.get('tag').then(function(result) {
+    tag = result.tag;
+    tagInput.value = tag;
+});
+
+// Gets the URL of the active tab and autofills the popup
+//queries for the active tab 
 browser.tabs.query({currentWindow: true, active: true}).then((tabs) => {
     // stores the tab
     var currentTab = tabs[0];
     // stores the URL of the active tab
-    currentUrl = tabs[0].url;
+    var currentUrl = tabs[0].url;
     // changes the value of the URL input the match the current URL
     urlInput.value = currentUrl;
 });
 
-
+// Gets the title, author, and date of an article (via its metadata) and fills each into the popup
 // sends a message to content script whenever the popup is opened
 browser.tabs.query({active: true, currentWindow: true}).then(function(tabs) {
 
-    // Send a message to the content script to retrieve the page title
+    // Send a message to the content script to retrieve the article title
     browser.tabs.sendMessage(tabs[0].id, {action: "getTitle"}).then(function(response) {
-        // Update the title input in the popup with the retrieved page title
+        // Update the title input in the popup with the retrieved article title
         title = response.value;
 
         titleInput.value = title;
         console.log(title);
     });
+    // sends a message to retrieve the author
     browser.tabs.sendMessage(tabs[0].id, {action: "getAuthor"}).then(function(response) {
         // Update the author input 
         author = response.value;
         authorInput.value = author;
         console.log(author);
     });
+    // sends a message to retrieve the date
     browser.tabs.sendMessage(tabs[0].id, {action: "getDate"}).then(function(response) {
         // Update the date input 
         date = response.value;
@@ -52,11 +65,6 @@ browser.tabs.query({active: true, currentWindow: true}).then(function(tabs) {
     });
 });
 
-//fills in any of the input slots with any information found 
-authorInput.value = author;
-dateInput.value = date;
-titleInput.value = title;
-urlInput.value = currentUrl;
 
 //whenever the cutButton button is clicked, it runs copyCard function
 cutButton.addEventListener("click", (event) =>  {
@@ -86,12 +94,12 @@ function getSelectedText() {
 function createCard() {
     
     let cites = '';
-    //gets the values of the html elements and stores them
-    author = authorInput.value
-    date = dateInput.value;
-    title = titleInput.value;
-    tag = tagInput.value;
-    currentUrl = urlInput.value;
+    //gets the values of the html input elements and stores them
+    var author = authorInput.value
+    var date = dateInput.value;
+    var title = titleInput.value;
+    var tag = tagInput.value;
+    var url = urlInput.value;
     //creates a date and formats it
     var currentDate = new Date();
     var formattedDate = formatDate(currentDate);
@@ -99,7 +107,7 @@ function createCard() {
 
     //concatenates information into the cite in the following format:
     // Name (Date) "Title", url. Accessed on Date2. Tag
-    cites += author + " (" + date + ") \"" + title + "\", " + currentUrl + ". Accessed on " + formattedDate + 
+    cites += author + " (" + date + ") \"" + title + "\", " + url + ". Accessed on " + formattedDate + 
             ". //" + tag;
     
     //consolidates the cites and the evidence under a card
@@ -131,54 +139,5 @@ function formatDate(date)  {
     var formattedDate = date.toLocaleDateString('en-US', options);
     return formattedDate;
 }
-
-
-
-/*
-//tries to automatically get the author's name via metadata
-// if it doesn't work, it just goes back to what the old name was
-function getAuthor(currentTab)    {
-
-    if (!currentTab) {
-        console.error("currentTab is not defined");
-        return Promise.reject(new Error("currentTab is not defined"));
-    }
-
-    const defaultAuthor = "Balls";
-
-
-    // Try to get the author from a meta tag
-    const metaCode = 'document.querySelector("meta[name=\'author\']").getAttribute("content")';
-    const metaPromise = browser.tabs.executeScript(currentTab.id, { code: metaCode })
-        .then(result => result[0])
-        .catch(error => console.error(error));
-
-    // Try to get the author from the Open Graph protocol
-    const ogCode = 'document.querySelector("meta[property=\'og:article:author\']").getAttribute("content")';
-    const ogPromise = browser.tabs.executeScript(currentTab.id, { code: ogCode })
-        .then(result => result[0])
-        .catch(error => console.error(error));
-
-    // Try to get the author from the article element
-    const articleCode = 'document.querySelector("article [itemprop=\'author\']")';
-    const articlePromise = browser.tabs.executeScript(currentTab.id, { code: articleCode })
-        .then(result => result[0].textContent.trim())
-        .catch(error => console.error(error));
-
-    // Wait for all promises to resolve and get the first successful result
-    Promise.all([metaPromise, ogPromise, articlePromise])
-        .then(results => {
-        const foundAuthor = results.find(result => result != null && result != "");
-        return foundAuthor || defaultAuthor; // or do something else with the author
-        })
-        .catch(error => {
-            console.error(error);
-            return defaultAuthor;
-        });
-}*/
-
-
-
-
 
 
